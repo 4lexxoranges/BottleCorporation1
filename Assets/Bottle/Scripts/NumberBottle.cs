fbb3ea2dc0188ebd53a67cc389b1a853f0133e75
4lexxoranges <or4nges.official@gmail.com>
2020-10-17 17:29:53 +0300

    add new to menu 90%100
    add new to menu 90%100

---
+++ Assets/Bottle/Scripts/NumberBottle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberBottle : MonoBehaviour
+{
+    private int numberBottle;
+
+    public GameObject bottle1;
+    public GameObject bottle2;
+    public GameObject bottle3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberBottle = PlayerPrefs.GetInt("Bottle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numberBottle == 1)
+        {
+            bottle1.SetActive(true);
+            bottle2.SetActive(false);
+            bottle3.SetActive(false);
+        }
+        if (numberBottle == 2)
+        {
+            bottle1.SetActive(false);
+            bottle2.SetActive(true);
+            bottle3.SetActive(false);
+        }
+        if (numberBottle == 3)
+        {
+            bottle1.SetActive(false);
+            bottle2.SetActive(false);
+            bottle3.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Bottle/Scripts/ShopBottle.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShopBottle : MonoBehaviour
+{
+    public GameObject tick1;
+    public GameObject tick2;
+    public GameObject tick3;
+    private int NumberBottle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NumberBottle = PlayerPrefs.GetInt("Bottle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(NumberBottle == 1)
+      {
+            tick1.SetActive(true);
+            tick2.SetActive(false);
+            tick3.SetActive(false);
+      }
+        if (NumberBottle == 2)
+        {
+            tick1.SetActive(false);
+            tick2.SetActive(true);
+            tick3.SetActive(false);
+        }
+        if (NumberBottle == 3)
+        {
+            tick1.SetActive(false);
+            tick2.SetActive(false);
+            tick3.SetActive(true);
+        }
+    }
+    public void Bottle1()
+    {
+        NumberBottle = 1;
+        PlayerPrefs.SetInt("Bottle", NumberBottle);
+    }
+    public void Bottle2()
+    {
+        NumberBottle = 2;
+        PlayerPrefs.SetInt("Bottle", NumberBottle);
+    }
+    public void Bottle3()
+    {
+        NumberBottle = 3;
+        PlayerPrefs.SetInt("Bottle", NumberBottle);
+    }
+}
